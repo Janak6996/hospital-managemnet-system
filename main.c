dad149881f4ad4682c6afc055f6c54b4a12d512e
Janak6996 <janakchhatkuli1234@gmail.com>
2024-04-21 01:38:05 +0545

    initial commit

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "makechoice.h"
+#include "adding_info.h"
+#include "viewing_info.h"
+#include "searching_info.h"
+#include "editting_info.h"
+#include "deleting_info.h"
+int main()
+{
+	int user,num,i;
+	top:
+	user=choices();
+	switch (user)
+	{
+		case 0:
+			break;
+		case 1:
+			system("cls");
+			printf("How many data do you have?");
+			scanf("%d",&num);
+			for(i=0;i<num;i++)
+			{
+			add();
+			}
+			main();
+			break;
+		case 2:
+			view();
+			break;
+		case 3:
+			search();
+			break;
+		case 4:
+			edit();
+			break;
+		case 5:
+			delete();
+			break;
+		default:
+			goto top;
+			break;
+	}
+}
